491dc6e4ca38fbe90467f02750390d71d0d84d9e
drmilosjovanovic <dr.milosjovanovic87@gmail.com>
2022-07-15 18:08:19 +0200

    Contact & About us stranice

--- lekcija-27/src/Views/contact.php
+++ lekcija-27/src/Views/contact.php
@@ -1,14 +1,24 @@
-<div class="content">
-    <h1>This is contact page</h1>
+<div class="container">
+    <div class="about-us">
+        <div class="contact-text">
+            <h1>Let's chat. </br> Tell us about your problem. </h1>
+            <p>Let's solve something together 🤟</p>
+        </div>
 
-    <form action="index.php?page=contact" method="POST">
-        Name:<br />
-        <input type="text" name="name" placeholder="Enter name"/><br />  
-        Message:<br />
-        <textarea name="message" placeholder="Enter text message " class="new-ad"></textarea><br />
-        
-        <div class="error"><?php echo $errors;?></div>
-        
-        <input type="submit" value="Send"/><br />
-    </form>
+        <div class="form">
+            <p>Send us a message🚀</p>
+            <form action="index.php?page=contact" method="POST">
+                Name:<br />
+                <input class="contact-name" type="text" name="name" placeholder="Enter name"/><br />  
+                Message:<br />
+                <textarea class="contact-message" name="message" placeholder="Enter text message " class="new-ad"></textarea><br />
+                
+                <div class="error"><?php echo $errors;?></div>
+                
+                <input id="send" type="submit" value="Send"/><br />
+            </form>
+
+
+        </div>
+    </div>
 </div>
